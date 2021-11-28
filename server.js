4db204f18f47fb2618ddf8af7aa536e1acb6b2c7
angrysrijeeta <srijeetadas5@gmail.com>
2021-11-28 22:13:43 +0400

    connected to mongodb

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
+
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const connectionString = "mongodb+srv://srijeetadas72:gungundas@cluster1.voqfk.mongodb.net/tets?retryWrites=true&w=majority"
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+  })
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
     // Note: __dirname is the current directory you're in. Try logging it and see what you get!
     // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
   })
   app.post('/quotes', (req, res) => {
-    console.log('Hellooooooooooooooooo!')
+    console.log(req.body)
   })
 app.listen(3000, function() {
     console.log('listening on 3000')
