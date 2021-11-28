835861c78684041e9868b5f9d22b351bd4f1c036
angrysrijeeta <srijeetadas5@gmail.com>
2021-11-28 20:20:51 +0400

    handle post requests

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.get('/', (req, res) => {
     // Note: __dirname is the current directory you're in. Try logging it and see what you get!
     // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
   })
+  app.post('/quotes', (req, res) => {
+    console.log('Hellooooooooooooooooo!')
+  })
 app.listen(3000, function() {
     console.log('listening on 3000')
   })
